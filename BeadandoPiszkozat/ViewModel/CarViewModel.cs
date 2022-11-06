6c44de515e8ad3b90b24489ca2e345d8cbf5470d
CrossRaccoon <grend4@gmail.com>
2022-11-06 12:38:16 +0100

    Datagrid readonly, datagrid to textblock binding

--- BeadandoPiszkozat/ViewModel/CarViewModel.cs
+++ BeadandoPiszkozat/ViewModel/CarViewModel.cs
@@ -9,137 +9,148 @@ using System.Threading.Tasks;
 
 namespace BeadandoPiszkozat.ViewModel
 {
-	public class CarViewModel : BaseViewModel
-	{
-		int _id;
-		string _brand;
-		string _model;
-		string _fuel;
-		string _maxPassenger;
-		string _numberOfDoors;
-		string _availableType;
-		string _price;
-	
-		public int IdofCars {
-			get { return _id; }
-			set { _id = value; }
-		}
-		public string Brand {
-			get { return _brand; }
-			set { _brand = value; }
-		}
-		public string Model {
-			get { return _model; }
-			set { _model = value; }
-		}
-		public string Fuel {
-			get { return _fuel; }
-			set { _fuel = value; }
-		}
-		public string MaxPassenger {
-			get { return _maxPassenger; }
-			set { _maxPassenger = value; }
-		}
-		public string NumberOfDoors {
-			get { return _numberOfDoors; }
-			set { _numberOfDoors = value; }
-		}
-		public string AvailableType {
-			get { return _availableType; }
-			set { _availableType = value; }
-		}
-		public string Price {
-			get { return _price; }
-			set { _price = value; }
-		}
-		public ObservableCollection<CarsDTO> Cars { get; set; }
-		public CarsDTO CurrentCar { get; set; }
+    public class CarViewModel : BaseViewModel
+    {
+        int _id;
+        string _brand;
+        string _model;
+        string _fuel;
+        string _maxPassenger;
+        string _numberOfDoors;
+        string _availableType;
+        string _price;
 
-		private CarService CarService;
-		public string Message { get; set; }
-		public RelayCommand SaveCommand { get; }
-		public RelayCommand UpdateCommand { get; }
-		public RelayCommand DeleteCommand { get; }
-		public RelayCommand SearchCommand { get; }
-		public CarViewModel()
-		{
-			CarService = new CarService();
-			LoadCars();
-			CurrentCar = new CarsDTO();
-			SaveCommand = new RelayCommand(save);
-			UpdateCommand = new RelayCommand(update);
-			DeleteCommand = new RelayCommand(delete);
-			SearchCommand = new RelayCommand(search);
-		}
+        public int IdofCars
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        public string Fuel
+        {
+            get { return _fuel; }
+            set { _fuel = value; }
+        }
+        public string MaxPassenger
+        {
+            get { return _maxPassenger; }
+            set { _maxPassenger = value; }
+        }
+        public string NumberOfDoors
+        {
+            get { return _numberOfDoors; }
+            set { _numberOfDoors = value; }
+        }
+        public string AvailableType
+        {
+            get { return _availableType; }
+            set { _availableType = value; }
+        }
+        public string Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public ObservableCollection<CarsDTO> Cars { get; set; }
 
-		private void LoadCars()
-		{
-			Cars = new ObservableCollection<CarsDTO>(CarService.getAll());
-		}
+        public CarsDTO CurrentCar { get; set; }
 
-		private void save()
-		{
-			try
-			{
-				bool isSaved = CarService.add(CurrentCar);
-				LoadCars();
-			}
-			catch (Exception ex)
-			{
+        public ObservableCollection<CarsDTO> Selected_Row { get; set; } //Viewban selected row a datagridből
 
-				throw ex;
-			}
-		}
+        private CarService CarService;
+        public string Message { get; set; }
+        public RelayCommand SaveCommand { get; }
+        public RelayCommand UpdateCommand { get; }
+        public RelayCommand DeleteCommand { get; }
+        public RelayCommand SearchCommand { get; }
+        public CarViewModel()
+        {
+            CarService = new CarService();
+            LoadCars();
+            CurrentCar = new CarsDTO();
+            SaveCommand = new RelayCommand(save);
+            UpdateCommand = new RelayCommand(update);
+            DeleteCommand = new RelayCommand(delete);
+            SearchCommand = new RelayCommand(search);
+        }
 
-		private void update()
-		{
-			try
-			{
-				var isUpdated = CarService.update(CurrentCar);
-				LoadCars();
-			}
-			catch (Exception ex)
-			{
+        private void LoadCars()
+        {
+            Cars = new ObservableCollection<CarsDTO>(CarService.getAll());
+        }
+        
+        private void save()
+        {
+            try
+            {
+                bool isSaved = CarService.add(CurrentCar);
+                LoadCars();
+            }
+            catch (Exception ex)
+            {
 
-				throw ex;
-			}
-		}
+                throw ex;
+            }
+        }
 
-		private void delete()
-		{
-			try
-			{
-				var isDeleted = CarService.delete(CurrentCar.Id);
-				LoadCars();
-			}
-			catch (Exception ex)
-			{
+        private void update()
+        {
+            try
+            {
+                var isUpdated = CarService.update(CurrentCar);
+                LoadCars();
+            }
+            catch (Exception ex)
+            {
 
-				throw ex;
-			}
-		}
+                throw ex;
+            }
+        }
 
-		private void search()
-		{
-			try
-			{
-				CarsDTO cars = CarService.search(CurrentCar.Id);
-				if (cars != null)
-				{
-					CurrentCar.Brand = cars.Brand;
-					CurrentCar.Model = cars.Model;
-					CurrentCar.Fuel = cars.Fuel;
-					CurrentCar.MaxPassenger = cars.MaxPassenger;
-					CurrentCar.NumberOfDoors = cars.NumberOfDoors;
-					CurrentCar.AvailableType = cars.AvailableType;
-					CurrentCar.Price = cars.Price;
-				}
-			}
-			catch (Exception ex)
-			{
+        private void delete()
+        {
+            try
+            {
+                var isDeleted = CarService.delete(CurrentCar.Id);
+                LoadCars();
+            }
+            catch (Exception ex)
+            {
 
-				throw ex;
-			}
-		}
-	}
+                throw ex;
+            }
+        }
+
+        private void search()
+        {
+            try
+            {
+                CarsDTO cars = CarService.search(CurrentCar.Id);
+                if (cars != null)
+                {
+                    CurrentCar.Brand = cars.Brand;
+                    CurrentCar.Model = cars.Model;
+                    CurrentCar.Fuel = cars.Fuel;
+                    CurrentCar.MaxPassenger = cars.MaxPassenger;
+                    CurrentCar.NumberOfDoors = cars.NumberOfDoors;
+                    CurrentCar.AvailableType = cars.AvailableType;
+                    CurrentCar.Price = cars.Price;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
 }
--- BeadandoPiszkozat/ViewModel/LoginViewModel.cs
+++ BeadandoPiszkozat/ViewModel/LoginViewModel.cs
@@ -11,86 +11,87 @@ using System.Windows;
 
 namespace BeadandoPiszkozat.ViewModel
 {
-	public class LoginViewModel : BaseViewModel
-	{
-		private string _loginName;
-		private string _password;
+    public class LoginViewModel : BaseViewModel
+    {
+        private string _loginName;
+        private string _password;
 
-		public static string _type;
-		public string LoginName
-		{
-			get { return _loginName; }
-			set { _loginName = value; }
-		}
-		public string Password
-		{
-			get { return _password; }
-			set { _password = value; }
-		}
-		public RelayCommand EnterLogin { get; }
-		public LoginViewModel()
-		{
-			EnterLogin = new RelayCommand(Mehet);
-		}
+        public static string _type;
+        public string LoginName
+        {
+            get { return _loginName; }
+            set { _loginName = value; }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public RelayCommand EnterLogin { get; }
+        public LoginViewModel()
+        {
+            EnterLogin = new RelayCommand(Mehet);
+        }
 
-		private void GetType()
-		{
-			try
-			{
-				using (var db = new ProbaLoginDBEntities())
-				{
-					var querry = from n in db.Login
-								 where n.Name.Equals(_loginName)
-								 select n;
+        private void GetType()
+        {
+            try
+            {
+                using (var db = new ProbaLoginDBEntities())
+                {
+                    var querry = from n in db.Login
+                                 where n.Name.Equals(_loginName)
+                                 select n;
 
-					foreach (var item in querry)
-					{
-						_type = item.Type;
-					}
-				}
-			}
-			catch (Exception)
-			{
+                    foreach (var item in querry)
+                    {
+                        _type = item.Type;
+                    }
+                }
+            }
+            catch (Exception)
+            {
 
-				throw;
-			}
+                throw;
+            }
 
-		}
-		void Mehet()
-		{
-			SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-NH9MBOU; Initial Catalog=ProbaLoginDB; Integrated Security=true;");
-			try
-			{
-				if (sqlCon.State == System.Data.ConnectionState.Closed)
-				{
-					sqlCon.Open();
-				}
-				string query = "SELECT COUNT(1) FROM Login WHERE Name=@Username AND Password=@Password";
-				SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
-				sqlCmd.CommandType = System.Data.CommandType.Text;
-				sqlCmd.Parameters.AddWithValue("@Username", _loginName);
-				sqlCmd.Parameters.AddWithValue("@Password", _password);
-				int counts = Convert.ToInt32(sqlCmd.ExecuteScalar());
-				if (counts == 1)
-				{
-					GetType();
-					Main m = new Main();
-					m.Show();
-					var asd = Application.Current.MainWindow;
-					asd.Close();
-				}
-				else
-					MessageBox.Show("Hibás név vagy jelszó.");
-			}
-			catch (Exception ex)
-			{
+        }
+        void Mehet()
+        {
+            //SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-NH9MBOU; Initial Catalog=ProbaLoginDB; Integrated Security=true;"); 
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-VQ5POJE; Initial Catalog=ProbaLoginDB; Integrated Security=true;");
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                string query = "SELECT COUNT(1) FROM Login WHERE Name=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = System.Data.CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", _loginName);
+                sqlCmd.Parameters.AddWithValue("@Password", _password);
+                int counts = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (counts == 1)
+                {
+                    GetType();
+                    Main m = new Main();
+                    m.Show();
+                    var asd = Application.Current.MainWindow;
+                    asd.Close();
+                }
+                else
+                    MessageBox.Show("Hibás név vagy jelszó.");
+            }
+            catch (Exception ex)
+            {
 
-				MessageBox.Show(ex.Message);
-			}
-			finally
-			{
-				sqlCon.Close();
-			}
-		}
-	}
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+    }
 }
